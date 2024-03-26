24a40f2f454fe35818140762ec8bf7c2edbfcb21
Dennis <ngetichdennis90@gmail.com>
2024-03-26 09:30:11 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
+# book.py
 
 class Book:
-    pass
+    '''Class representing a book'''
+
+    def __init__(self, title, page_count):
+        '''Initialize Book with title and page_count'''
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        '''Simulate turning a page'''
+        print("Flipping the page...wow, you read fast!")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
+# shoe.py
+
 class Shoe:
-    pass
+    '''Class representing a shoe'''
+
+    def __init__(self, brand, size):
+        '''Initialize Shoe with brand and size'''
+        self.brand = brand
+        self.size = size
+
+    def cobble(self):
+        '''Simulate cobbling a shoe'''
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
